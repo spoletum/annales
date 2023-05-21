3efce9226f9b7c670d12333c657838bf5e349337
Alessandro Santini <alessandro.santini@toremo.com>
2023-05-21 09:06:21 +0200

    Added log-level option plus refactoring

--- cmd/annales.go
+++ cmd/annales.go
@@ -24,6 +24,7 @@ const (
 	flagGrpcAddr  = "grpc-addr"
 	flagHttpAddr  = "http-addr"
 	flagLogFormat = "log-format"
+	flagLogLevel  = "log-level"
 	flagDbUrl     = "db-url"
 )
 
@@ -49,6 +50,11 @@ func main() {
 								Name:  flagHttpAddr,
 								Usage: "Address to bind the HTTP/2 endpoint",
 								Value: "0.0.0.0:9000",
+							},
+							&cli.StringFlag{
+								Name:  flagLogLevel,
+								Usage: "Specify the threshold level of logging",
+								Value: "info",
 							},
 							&cli.StringFlag{
 								Name:  flagLogFormat,
@@ -81,19 +87,12 @@ func startServer(ctx *cli.Context) error {
 	var driver annales.JournalServer
 
 	// Extract the flags
-	logFormat := ctx.String(flagLogFormat)
 	httpAddr := ctx.String(flagHttpAddr)
-	grpcAddr := ctx.String(flagGrpcAddr)
 	dbUrl := ctx.String(flagDbUrl)
 
-	// Setup the logger
-	switch logFormat {
-	case "json":
-		break
-	case "console":
-		log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Timestamp().Logger()
-	default:
-		return fmt.Errorf("--%s: value '%s' is not permitted (json, console)", flagLogFormat, logFormat)
+	// Setup logging
+	if err := setupLog(ctx); err != nil {
+		return err
 	}
 
 	// Display a welcome string
@@ -108,7 +107,7 @@ func startServer(ctx *cli.Context) error {
 	log.Info().Str("db-url", dbUrl).Msg("Establishing a connection with MongoDB")
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(dbUrl))
 	if err != nil {
-		log.Fatal().Str("db-url", dbUrl).Err(err).Msg("Error establishing a connection to mongodb")
+		log.Fatal().Str(flagDbUrl, dbUrl).Err(err).Msg("Error establishing a connection to mongodb")
 	}
 	defer func() {
 		_ = client.Disconnect(context.Background())
@@ -116,6 +115,9 @@ func startServer(ctx *cli.Context) error {
 
 	// TODO where do we get the database name from?
 	repo, err := repository.NewMongoRepository(context.Background(), client, "annales")
+	if err != nil {
+		return err
+	}
 
 	// Creates the caching journal
 	driver, err = journal.NewCachingJournal(repo, 100)
@@ -132,6 +134,60 @@ func startServer(ctx *cli.Context) error {
 		}
 	}()
 
+	if err := startGrpcEndpoint(ctx, driver, &wg); err != nil {
+		return err
+	}
+
+	// Waits for the two listeners to complete
+	wg.Wait()
+
+	return nil
+}
+
+// Sets up log level and log output
+func setupLog(ctx *cli.Context) error {
+
+	// Extract the parameters from the command line
+	flagLevel := ctx.String(flagLogLevel)
+	flagFormat := ctx.String(flagLogFormat)
+
+	var logLevel zerolog.Level
+
+	switch flagLevel {
+	case "debug":
+		logLevel = zerolog.DebugLevel
+	case "info":
+		logLevel = zerolog.InfoLevel
+	case "warn":
+		logLevel = zerolog.WarnLevel
+	case "error":
+		logLevel = zerolog.ErrorLevel
+	case "fatal":
+		logLevel = zerolog.FatalLevel
+	case "panic":
+		logLevel = zerolog.PanicLevel
+	default:
+		return fmt.Errorf("unknown log level %s", flagLevel)
+	}
+
+	// Setup the logger
+	switch flagFormat {
+	case "json":
+		break
+	case "console":
+		log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Timestamp().Logger().Level(logLevel)
+	default:
+		return fmt.Errorf("--%s: value '%s' is not permitted (json, console)", flagLogFormat, flagFormat)
+	}
+	return nil
+}
+
+// Starts the Grpc endpoint. the context provides the flags, wg needs to be informed when the goroutine terminates.
+func startGrpcEndpoint(ctx *cli.Context, instance annales.JournalServer, wg *sync.WaitGroup) error {
+
+	// Extract the GRPC bind address from the command line
+	grpcAddr := ctx.String(flagGrpcAddr)
+
 	// Start the GRPC endpoint in its own goroutine
 	go func() {
 		defer wg.Done()
@@ -145,14 +201,10 @@ func startServer(ctx *cli.Context) error {
 
 		// Configure and start the GRPC endpoint
 		server := grpc.NewServer()
-		annales.RegisterJournalServer(server, driver)
+		annales.RegisterJournalServer(server, instance)
 		if err := server.Serve(listener); err != nil {
 			log.Fatal().Err(err).Msg("Error starting the GRPC endpoint")
 		}
 	}()
-
-	// Waits for the two listeners to complete
-	wg.Wait()
-
 	return nil
 }
